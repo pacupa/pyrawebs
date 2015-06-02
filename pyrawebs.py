2d1dee8eec55e5f1a3bf2e0fb60d1cb6d892ca40
Marce Elizeche <melizeche@gmail.com>
2015-06-02 10:34:19 -0400

    Agregado listado de senadores, soporte unicode de tweets y OCD style fixes

--- pyrawebs.py
+++ pyrawebs.py
@@ -22,17 +22,17 @@ if __name__ == "__main__":
   api = oauth_login(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
   print "Authenticated as: %s" % api.me().screen_name
 
-  ltweets=[]
-  ldiputados=[]
+  ltweets     = []
+  ldiputados  = []
 
-  atweets= str(sys.argv[1])
-  adiputados = str(sys.argv[2])
+  atweets     = str(sys.argv[1])
+  adiputados  = str(sys.argv[2])
 
-  ftweets=open(atweets,'r')
-  fdiputados=open(adiputados,'r')
+  ftweets     = open(atweets,'r')
+  fdiputados  = open(adiputados,'r')
 
-  stweets=ftweets.readlines()
-  sdiputados=fdiputados.readlines()
+  stweets     = ftweets.readlines()
+  sdiputados  = fdiputados.readlines()
 
   for line in stweets:
     ltweets.append(line.strip())
@@ -43,15 +43,14 @@ if __name__ == "__main__":
   for diputado in ldiputados:
     random.shuffle(ltweets)
     for tweet in ltweets:
-      tweet=tweet.replace('@',diputado)
-      cadena=tweet
+      tweet   = tweet.decode('utf-8')
+      tweet   = tweet.replace('@',diputado)
+      cadena  = tweet
       print tweet
       try:
           api.update_status(status=cadena)
-          espera=randint(4,37)
+          espera = randint(4,37)
           time.sleep(espera)
           print 'espera: '+str(espera)+' segundos'
       except:
           print "Failed to tweet:", tweet
-
-
